131913d4e523bf4a6b9353be2d731700bf936c5a
jamlagz28 <jameslaag2003@gmail.com>
2024-04-18 15:13:43 +0800

    added

--- read.php
+++ read.php
@@ -1,24 +1,24 @@
 <?php
 // Check existence of id parameter before processing further
-if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+if(isset($_GET["product_id"]) && !empty(trim($_GET["product_id"]))){
     // Include config file
-    require_once "config.php";
+    require_once "../db/config.php";
     
-    // Prepare a select statement
-    $sql = "SELECT * FROM employees WHERE id = :id";
-    
+    // // Prepare a select statement
+    // $sql = "SELECT * FROM employees WHERE id = :id";
+    $sql = "SELECT * FROM products WHERE product_id = :product_id";
+
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":id", $param_id);
+        $stmt->bindParam(":product_id", $param_product_id);
         
         // Set parameters
-        $param_id = trim($_GET["id"]);
+        $param_product_id = trim($_GET["product_id"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             if($stmt->rowCount() == 1){
-                /* Fetch result row as an associative array. Since the result set
-                contains only one row, we don't need to use while loop */
+              
                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
                 
                 // Retrieve individual field value
